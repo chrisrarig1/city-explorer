7677e3d2abe572e17488a7cfb61acbb079eaae11
Chris Rarig <cwrarig20@gmail.com>
2021-10-05 22:04:22 -0500

    cleaned up file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+class  App extends React.Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      cityName: ''
+    }
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>enter a city</h1>
+    <input onChange= {(e) =>{this.setState({cityName: e.target.value})}} ></input>
+    </>
   );
+  }
 }
 
 export default App;
