3fe12530c375d77802876d95c0c167da73841253
Chris Rarig <cwrarig20@gmail.com>
2021-10-12 23:53:58 -0500

    added weather dat to page

---
+++ src/Weather.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+
+class Weather extends React.Component{
+
+
+     render(){
+         return(
+             <>
+             
+                               
+                <Card style={{ width: '18rem' }}>
+                    <Card.Body>
+                    
+                         <Card.Title> {this.props.date}</Card.Title> 
+                        <Card.Text>
+                        {this.props.description}
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                
+
+
+             </>
+         )
+     }
+
+}
+
+
+
+ export default Weather;
